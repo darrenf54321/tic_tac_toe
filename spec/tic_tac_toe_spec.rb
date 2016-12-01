027ee53c378e0fe003e897f7c68f2b0ebf35e927
Darrenf <darrenf54321@gmail.com>
2016-12-01 09:58:53 +0000

    winning combo test added and passing

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -8,5 +8,7 @@ describe TicTacToe do
     expect(game.show_array).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
   end
 
-
+  it "knows the potential winning combinations" do
+    expect(game.winner_combos). to eq([ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ])
+  end
 end

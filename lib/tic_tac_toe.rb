ade3e9ba7745ee19f2c7b52b18fabbc7e2590c5b
Darrenf <darrenf54321@gmail.com>
2016-12-01 10:19:16 +0000

    each player initialized with empty array

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,11 @@
+require 'player'
+
 class TicTacToe
 
   def initialize
     @board = (1..9).to_a
+    @player1 = []
+    @player2 = []
   end
 
   def show_array
@@ -12,6 +16,13 @@ class TicTacToe
     [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ]
   end
 
+  def player_1
+    @player1
+  end
+
+  def player_2
+    @player2
+  end
 
 
 
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -8,7 +8,17 @@ describe TicTacToe do
     expect(game.show_array).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
   end
 
+  it "starts with an empty array for player 1" do
+    expect(game.player_1).to eq([])
+  end
+
+  it "starts with an empty array for player 2" do
+    expect(game.player_2).to eq([])
+  end
+
   it "knows the potential winning combinations" do
     expect(game.winner_combos). to eq([ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ])
   end
+
+
 end

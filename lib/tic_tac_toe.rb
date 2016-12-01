528c032621f917b60c6d368733a4c8b4dbeae71c
Darrenf <darrenf54321@gmail.com>
2016-12-01 09:49:38 +0000

    restructured program

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,20 +1,12 @@
 class TicTacToe
 
   def initialize
-    @board = []
-      i = 1
-      9.times do
-        @board << "#{i}:  "
-        i += 1
-      end
+    @board = (1..9).to_a
   end
 
   def show_array
     @board
   end
 
-  def create_board
-    return "#{@board[0]}#{@board[1]}#{@board[2]}#{@board[3]}#{@board[4]}#{@board[5]}#{@board[6]}#{@board[7]}#{@board[8]}"
-  end
 
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -4,11 +4,9 @@ describe TicTacToe do
     subject(:game) { TicTacToe.new }
 
 
-  it "initializes with an erray of numbers 1 - 9" do
-    expect(game.show_array).to eq(["1:  ", "2:  ", "3:  ", "4:  ", "5:  ", "6:  ", "7:  ", "8:  ", "9:  "])
+  it "starts with an erray of numbers 1 - 9" do
+    expect(game.show_array).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
   end
 
-  it "creates an empty playing board" do
-    expect(game.create_board).to eq ("1:  2:  3:  4:  5:  6:  7:  8:  9:  ")
-  end
+
 end
